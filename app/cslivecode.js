2faa679d60049408ddf9b76c8757bbda509e9c70
veplaini <victor.lazzarini@nuim.ie>
2018-11-26 09:56:07 +0000

    added MIDI to app

--- app/cslivecode.js
+++ app/cslivecode.js
@@ -4,37 +4,59 @@ var initcs = false;
 
 // load scripts
 CsoundObj.importScripts("../js/").then(() => {
-        startCsound();
-        initcs = true;
- });
+    startCsound();
+    initcs = true;
+});
 
 
 function evalCode() {
-	cs.compileOrc(editor.getSelection());
+    cs.compileOrc(editor.getSelection());
 }
 
 
 let editor = CodeMirror.fromTextArea(
-	document.getElementById("csoundCodeEditor"), 
-	{
-		lineNumbers: true,
-    matchBrackets: true,
-    autoCloseBrackets: true,
-    theme: "monokai",
-    mode: "csound",
-		extraKeys: {
-			"Ctrl-E": evalCode,
-		},
-	});
+    document.getElementById("csoundCodeEditor"), 
+    {
+	lineNumbers: true,
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        theme: "monokai",
+        mode: "csound",
+	extraKeys: {
+	    "Ctrl-E": evalCode,
+	},
+    });
 
 
 function loadCSD(editor, csdFile) {
-  var client = new XMLHttpRequest();
-  client.open('GET', csdFile, true);
-  client.onreadystatechange = function() {
-    editor.setValue(client.responseText);
-  }
-  client.send();
+    var client = new XMLHttpRequest();
+    client.open('GET', csdFile, true);
+    client.onreadystatechange = function() {
+        editor.setValue(client.responseText);
+    }
+    client.send();
+}
+
+function WebMIDI_init(midi_handle) {
+    var haveDevs = false;
+    var devs = midi_handle.inputs.values();
+    for ( var m = devs.next(); m && !m.done; m = devs.next()) {
+        m.value.onmidimessage = onMIDIEvent;
+        haveDevs = true;
+    }
+    if (!haveDevs)
+        console.log("No MIDIDevs");
+    else {
+        console.log("WebMIDI support enabled");
+    }
+}
+
+function WebMIDI_err(err) {
+    console.log("Error starting WebMIDI");
+}
+
+function onMIDIEvent(event) {
+    cs.midiMessage(event.data[0], event.data[1], nevent.data[2]);
 }
 
 
@@ -44,15 +66,21 @@ function startCsound() {
     cs.start();  
     var ld = document.getElementById("loadDiv");
     if(ld != null) {
-      ld.remove();
+        ld.remove();
     }
     editor.refresh();
     editor.focus();
     editor.setCursor(0,0);
+
+    if (navigator.requestMIDIAccess)
+        navigator.requestMIDIAccess().then(WebMIDI_init,
+          WebMIDI_err);
+      else
+        console.log("No WebMIDI support");
 }
 
 // Prevent Refresh
 
 window.onbeforeunload = function() {
-  return "Are you...sure?" 
+    return "Are you...sure?" 
 };
